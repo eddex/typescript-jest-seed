2cb49d28ac719fc87e92d10531200ba9e52cdee7
eddex <eddex@outlook.com>
2019-09-03 15:30:47 +0200

    beautiful implementation of charles chat bot

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
 class Charles {
-  
-  // Your code here
 
+  hey(message: string) {
+
+    let isEmpty: boolean = message.trim() === '';
+    let isQuestion: boolean = message.trim().slice(-1) === '?';
+    let isAngry: boolean = message === message.toUpperCase();
+    let containsLetters: boolean = !!message.match(/[a-z]/i); // i = case insensitive.
+
+    return isEmpty ? 'Fine. Be that way!'
+      : isQuestion ? isAngry && containsLetters ? 'Calm down, I know what I\'m doing!' : 'Sure.'
+      : isAngry && containsLetters? 'Whoa, chill out!'
+      : 'Whatever.';
+  }
 }
 
 export default Charles
